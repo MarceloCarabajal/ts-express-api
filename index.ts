229476523afd990fec657b97fdee416c24380bc9
MarceloCarabajal <marce.tsgia@gmail.com>
2024-07-28 18:06:24 -0300

    Uso de librerias que no estan escritas en TypeScript

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  
+  res.status(201).json( {
+    ok: true,
+    msg: 'ID: ASDASDASD'
+  })
+
+  res.json({ msg: "Todo ok"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
